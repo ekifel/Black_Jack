80e439ed71e167e4534ebf9d4e5ad14ec243204d
ekifel <kifel.ernest@gmail.com>
2020-05-04 12:00:51 +0500

    Добавлен класс User

--- user.rb
+++ user.rb
@@ -1,18 +1,19 @@
 class User
-	attr_accessor :cards, :bank
-	attr_reader :name, :score
+	attr_accessor :cards, :score
+	attr_reader :name, :bank
 
 	def initialize(name)
 		@name = name
-		@bank = 100
+		@bank = 100		
 		@cards = []
+		@score = 0
 	end
 
-	def make_a_bet
-		@bank -= 10
+	def make_a_bet(bet)
+		@bank -= bet
 	end
 
-	def take_a_win
-		@bank += 10
+	def take_a_win(bank)
+		@bank += bank
 	end
 end
