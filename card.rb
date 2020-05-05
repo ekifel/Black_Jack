b1a7524c2e66ba4cd74257265623ac90a9f00e69
ekifel <kifel.ernest@gmail.com>
2020-05-05 16:29:08 +0500

    и еще одни правочки

--- card.rb
+++ card.rb
@@ -10,17 +10,4 @@ class Card
     @name = name
     @price = price
   end
-
-  def self.create_deck
-    deck = []
-    CARD_VALUES.each do |value|
-      CARD_SUITS.each do |suit|
-        price = value if (2..10).include?(value)
-        price = 10 if %w[J Q K].include?(value)
-        price = 11 if value == 'A'
-        deck << Card.new("#{value}#{suit}", price)
-      end
-    end
-    deck
-  end
 end
--- deck.rb
+++ deck.rb
@@ -6,10 +6,24 @@ class Deck
   attr_reader :cards
 
   def initialize
-    @cards = Card.create_deck
+    @cards = []
+    create_deck
   end
 
   def shuffle
     @cards.shuffle!
+  end
+
+  private
+
+  def create_deck
+    Card::CARD_VALUES.each do |value|
+      Card::CARD_SUITS.each do |suit|
+        price = value if (2..10).include?(value)
+        price = 10 if %w[J Q K].include?(value)
+        price = 11 if value == 'A'
+        @cards << Card.new("#{value}#{suit}", price)
+      end
+    end
   end
 end
